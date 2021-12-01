ce28660e3b43ab3c231111793a01487580a8819c
Andrew Burford <andrewsburford@gmail.com>
2021-12-01 12:04:38 -0500

    fix rotating_quorums by making ledger.speculate idempotent so syncmanager doesn't overwrite ledger states if you change leaders so that timeouts occur, things might still break

--- executor_configs/rotating_quorums.py
+++ executor_configs/rotating_quorums.py
@@ -4,6 +4,8 @@ test_cases = []
 rounds = [Round('a', [["a", "b", "c"], ["d"], ["d'"]], []) for _ in range(2)]
 rounds += [Round('a', [["a", "b", "d"], ["c"], ["d'"]], []) for _ in range(2)]
 rounds += [Round('a', [["a", "c", "d"], ["b"], ["d'"]], []) for _ in range(2)]
-rounds += [Round('a', [["b", "c", "d"], ["a"], ["d'"]], []) for _ in range(2)]
-gst = [Round('a', [["a", "b", "c", "d", "d'"]], []) for _ in range(7)]
+rounds += [Round('c', [["b", "c", "d"], ["a"], ["d'"]], []) for _ in range(2)]
+
+rounds += [Round('c', [["a", "b", "c", "d", "d'"]], []) for _ in range(1)]
+gst = [Round('c', [["a", "b", "c", "d", "d'"]], []) for _ in range(7)]
 test_cases.append(TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'test0', 30, len(rounds)))
