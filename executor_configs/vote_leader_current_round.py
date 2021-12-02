7753fca69722b8310cc10afae33f1145c081dc9f
Ethan DeTurk <ethan.deturk@stonybrook.edu>
2021-12-02 02:01:27 -0500

    Inject bug where votes are sent to the leader of the current round

---
+++ executor_configs/vote_leader_current_round.py
@@ -0,0 +1,10 @@
+from testconfig import *
+
+rounds = [Round('a', [["a", "b", "c", "d", "d'"]], []) for _ in range(2)]
+rounds += [Round('b', [["a", "b", "c", "d", "d'"]], []) for _ in range(2)]
+rounds += [Round('c', [["a", "b", "c", "d", "d'"]], []) for _ in range(2)]
+gst = [Round('a', [["a", "b", "c", "d", "d'"]], []) for _ in range(7)]
+test_cases = [
+    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'bug', 30, len(rounds), {'vote_leader_current_round'}),
+    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'no_bug', 30, len(rounds), {})
+]
