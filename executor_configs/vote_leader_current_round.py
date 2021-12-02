3bcc81c8f76f8cf046c496eea0a70af4e2c72ad3
Ethan DeTurk <ethan.deturk@stonybrook.edu>
2021-12-02 02:22:17 -0500

    Fix GST round in vote_leader_current_round config

--- executor_configs/vote_leader_current_round.py
+++ executor_configs/vote_leader_current_round.py
@@ -5,6 +5,6 @@ rounds += [Round('b', [["a", "b", "c", "d", "d'"]], []) for _ in range(2)]
 rounds += [Round('c', [["a", "b", "c", "d", "d'"]], []) for _ in range(2)]
 gst = [Round('a', [["a", "b", "c", "d", "d'"]], []) for _ in range(7)]
 test_cases = [
-    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'bug', 30, len(rounds), {'vote_leader_current_round'}),
-    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'no_bug', 30, len(rounds), {})
+    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'bug', 30, 0, {'vote_leader_current_round'}),
+    TestConfig(['a','b','c','d'], ["d'"], rounds + gst, 0.8, 0, 'no_bug', 30, 0, {})
 ]
